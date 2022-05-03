6dedb556be35827dc70ce9df0867aa71d899183e
Yue Hao <haoyuethink@gmail.com>
2022-05-03 12:36:02 -0700

    Add files via upload

---
+++ workflow/2_aggregate_results.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+####################################
+# normalize your dir to the root level of the repo
+function canonicalPath
+{
+    local path="$1" ; shift
+    if [ -d "$path" ]
+    then
+        echo "$(cd "$path" ; pwd)"
+    else
+        local b=$(basename "$path")
+        local p=$(dirname "$path")
+        echo "$(cd "$p" ; pwd)/$b"
+    fi
+}
+####################################
+# usage example
+mycanonicalpath=$(canonicalPath "$../")
+
+
+# Your code start from here:
+multiqc cache/*fastqc.zip --outdir output
