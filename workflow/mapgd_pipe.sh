8708b139aa4e3449e3a58e8c3ad639086ec6150b
Yue Hao <haoyuethink@gmail.com>
2022-05-03 17:47:49 -0700

    Update mapgd_pipe.sh

--- workflow/mapgd_pipe.sh
+++ workflow/mapgd_pipe.sh
@@ -40,17 +40,28 @@ samtools view -H Alc-0.sort.bam > Arabidopsis.header
 # Generate mpileup file
 samtools mpileup -q 25 -Q 25 -B Alc-0.filtered.sort.bam Jea.filtered.sort.bam Oy-0.filtered.sort.bam Ri-0.filtered.sort.bam Sakata.filtered.sort.bam > Arabidopsis.mpileup
 
-# Generate mpileup file
-samtools mpileup -q 25 -Q 25 -B Alc-0.filtered.sort.bam Jea.filtered.sort.bam Oy-0.filtered.sort.bam Ri-0.filtered.sort.bam Sakata.filtered.sort.bam > Arabidopsis.mpileup
-
 # Make a pro file of nucleotide-read quartets (counts of A, C, G, and T) from the mpileup file
 mapgd proview -i Arabidopsis.mpileup -H Arabidopsis.header > Arabidopsis.pro
 
 # Run the allele command to estimate allele and genotype frequencies from the pro file.
-mapgd allele -i Arabidopsis.pro -o Arabidopsis
+mapgd allele -i Arabidopsis.pro -o Arabidopsis -p Arabidopsis.clean
+# .map file headline:
+#@SCFNAME    	POS	REF	MAJOR	MINOR	COVERAG	MJ_FREQ	VR_FREQ	ERROR	NULL_ER	NULL_E2	F_STAT	MM_FREQ	Mm_FREQ	mm_FREQ	HETERO	POLY_LR	HWE_LR	GOF	EF_CHRM	IND_INC	IND_CUT	BEST_LL
+
+# Determine the minimum and maximum population-coverage cut-off values by making a histogram of the population coverage.
+awk '{print $6}' Arabidopsis.map > Arabidopsis_coverage.txt
+# Make the coverage histogram in R.
+
+# Run the filter command to filter the map file of ML estimates of the parameters.
+mapgd filter -i Arabidopsis.map -p 20 -q 0.05 -Q 0.45 -c 10 -C 250 -o filtered_Arabidopsis.map
+#-p: minimum value of the likelihood-ratio test statistic for polymorphism 
+#-q: minimum minor-allele frequency estimate
+#-Q: maximum minor-allele frequency estimate
+#-c: minimum population coverage
+#-C: maximum population coverage
 
 # Run the genotype command to generate a file of genotype likelihoods.
-mapgd genotype -p Arabidopsis.pro -m Arabidopsis.map > Arabidopsis.genotype
+mapgd genotype -p Arabidopsis.clean.pro -m filtered_Arabidopsis.map > Arabidopsis.genotype
 
 # Run the relatedness command.
-mapgd relatedness -i Arabidopsis.genotype -o Arabidopsis.rel
+mapgd relatedness -i Arabidopsis.genotype -o Arabidopsis
